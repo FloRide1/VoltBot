e9681e88d3e45bd4aedefc4279d104c7cb77ceed
FloRide1 <florian.reimat@orange.fr>
2022-07-21 15:32:53 +0200

    [MAIN] Add config + credential check

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import config from "./config/config"
+
 function main(): void
 {
     console.log("VoltBolt - Hello World");
 
-    // Get Credentials
+    // Credentials
+    if (!check_crendentials())
+        console.log("[ERROR] Credentials are not correctly setup");
+}
+
+function check_crendentials(): boolean
+{
+    if (config.discord.token === "0")
+        return false;
+    return true;
 }
 
 main();
