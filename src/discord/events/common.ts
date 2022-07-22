fcee1c52a981073bc3129cc913704007d816765d
FloRide1 <florian.reimat@orange.fr>
2022-07-22 18:55:25 +0200

    [DISCORD] Add reaction removal

--- src/discord/events/common.ts
+++ src/discord/events/common.ts
@@ -2,6 +2,7 @@ import type {ArgsOf} from "discordx";
 import {Discord,
         On} from "discordx";
 import check_text from "../../spellcheck/spellcheck.js";
+import config from "../../config/config.js";
 
 @Discord()
 export abstract class AppDiscord
@@ -14,6 +15,7 @@ export abstract class AppDiscord
             if (res.corrections.length != 0)
             {
                 console.log(`[BOT] The Message "${message.content}"(${message.id}) is misspelled: ${res.corrections[0].explanation}`);
+                message.react(config.discord.emoji);
             }
         });
     }
@@ -28,6 +30,12 @@ export abstract class AppDiscord
             if (res.corrections.length != 0)
             {
                 console.log(`[BOT] The Updated Message "${new_message.content}"(${new_message.id}) is misspelled: ${res.corrections[0].explanation}`);
+                new_message.react(config.discord.emoji);
+            }
+            else if (new_message.reactions.cache.map(r => r.me).some(x => x))
+            {
+                console.log(`[BOT] The Updated Message "${new_message.content}"(${new_message.id}) is corrected`);
+                new_message.reactions.cache.get(config.discord.emoji)?.remove();
             }
         });
     }
